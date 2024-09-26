61a7d6efc82d627e6863fa6afcb8a5d3c72f259d
Lucy <lucy@example.com>
2024-09-26 11:13:52 +0800

    getAATable draft, all commented out. Inst excel draft uploaded

---
+++ R/getAATable.R
@@ -0,0 +1,13 @@
+#"fraction" make all "main Parameters"?
+#"name" full name, include standards, ensure how standards are names is the same first.    
+#"abbr" could have 3 letter abbreviations. maybe take from HMDB?
+#"id"  different from abbr? 
+#"type" is this as lipo is "prediction" or should it be quantified/ semi-quantified?    
+#"value" will be blank
+#"unit" take from SOP  (uM)
+#"refMax" lipo comes from biological upper limit provided by bruker, apparently comes from 100 people, who were clinical trials. 
+#"refMin" take from SOP from sharepoint using the lowest cal value (1)
+#"refUnit" different from unit? or blank? (uM)
+
+#could have version as reference to the SOP document where the QC value for refMin is taken
+
