d0b0d03f7ab92ce58b718dc4be63b926be1ebfcd
Lucy <lucy@example.com>
2024-10-01 14:15:38 +0800

    add extra amino acids file for newer cohort, add tests for readAA and update sampleID creation for truly unique sampleID for Cal and QC samples

--- R/readAA.R
+++ R/readAA.R
@@ -1,11 +1,11 @@
 #' Read amino acid data from tsv's
 #'
 #' @param file - the file path to be imported
-#' @param codePosition - position of the code in the file name
+#' @param codePosition - position of the code in the file name for sampleIDs. 
 #' @param optns list of options
 #' \itemize{
-#'    \item codePosition - position of the code in the file name. Default is 7
-#'    \item columnList - list of columns to be selected
+#'    \item codePosition - position of the code in the file name. Default is 
+#'    where cal is found in the analysis name.
 #' }
 #' @return rawData read from TSV file
 #'
@@ -17,123 +17,146 @@
 readAA <- function(file, optns = list()) {
 
 ####read in the file####
-  
-  rawData <- read.delim2(file = file,
-                         fileEncoding = "latin1",
-                         header = TRUE,
-                         check.names = FALSE)
-  
-  if (nrow(rawData) == 0 || ncol(rawData) == 0) {
-    warning(paste("file", file, "is empty"))
-    return(invisible(NULL))  # Return invisible NULL to allow further execution
-  }
-  
-  cat(paste("fusion:", nrow(rawData),
-            "line(s) read\n"))
-  
-  fi <- !is.na(rawData$AnalyteName)
-  rawData <- rawData[fi,]
-  cat(paste("fusion:",
-            sum(!fi),
-            "empty line(s) removed\n"))
-  
-  ####### get sampleID position in title#######
-  if ("codePosition" %in% names(optns)) {
-    codePosition <- optns$codePosition
-  } else {
-    codePosition <- 7
-  }
-  
-  #####number of compounds############
-  compoundList <- unique(rawData$AnalyteName)
-  numberOfCompounds <- length(compoundList)
-  cat(paste("fusion:",
-            numberOfCompounds,
-            "compound(s) found\n"))
-  
-  #######number of samples############
-  cat(paste("fusion:",
-            length(unique(rawData$AnalysisName)),
-            "sample(s) found\n"))
-  
-  ###### looking for duplicated lines########
-  idx <- which(duplicated(rawData[, which(colnames(rawData) %in% c("AnalysisName", "AnalyteName"))]) |
-                 duplicated(rawData[,which(colnames(rawData) %in% c("AnalysisName", "AnalyteName"))], fromLast = TRUE))
-  
-  if (length(idx) > 0) {
-    cat(crayon::red("fusion: " %+%
-                      crayon::red(idx) %+%
-                      crayon::red(" duplicated line(s) found")),
-        fill = TRUE)
-  }
-  idx <- which(duplicated(rawData[,which(colnames(rawData) %in% c("AnalysisName", "AnalyteName"))]))
-  
-  if (length(idx) > 0) {
-    rawData <- rawData[-idx,]
-    cat(crayon::white("fusion: " %+%
-                        crayon::white(idx) %+%
-                        crayon::white(" duplicated line(s) removed")),
-        fill = TRUE)
-  }
-  
-  #############adding sampleID########
-  #double blanks and LTRs need further individualizing since they are not unique yet
-  
-  rawData$sampleID <- sapply(rawData$AnalysisName, function(name) {
-    # Split by "_"
-    parts <- strsplit(name, "_")[[1]]
-    
-    # Check if "Blank" or "LTR" is in the split parts
-    if (any(grepl("Blank|LTR", parts))) {
-      # Find the position of "Blank" or "LTR" and take the part with the number after it
-      idx <- which(grepl("Blank|LTR", parts))
-      return(paste(parts[idx], parts[idx + 1], sep = "_"))  # Combine "Blank" or "LTR" with the next number
-    } else {
-      # For all other cases, use the default code extraction
-      return(parts[codePosition])  # Adjust 'codePosition' as needed
+
+    rawData <- read.delim2(file = file,
+                           fileEncoding = "latin1",
+                           header = TRUE,
+                           check.names = FALSE)
+
+    if (nrow(rawData) == 0 || ncol(rawData) == 0) {
+      warning(paste("file", file, "is empty"))
+      return(invisible(NULL))  # Return invisible NULL to allow further execution
     }
-  })
-  
-  
-  #############cleaning sampleType##########
-  #rename column correctly
-  colnames(rawData)[which(colnames(rawData) == "SampleType")] <- "sampleType"
-  
-  idx <- grep("blank", tolower(rawData$sampleID))
-  rawData$sampleType[idx] <- "blank"
-  
-  idx <- grep("sltr", tolower(rawData$sampleID))
-  rawData$sampleType[idx] <- "sltr"
-  
-  idx <- grep("^ltr", tolower(rawData$sampleID))
-  rawData$sampleType[idx] <- "ltr"
-  
-  idx <- grep("^pqc", tolower(rawData$sampleID))
-  rawData$sampleType[idx] <- "pqc"
-  
-  idx <- grep("^qc", tolower(rawData$sampleID))
-  rawData$sampleType[idx] <- "qc"
-  
-  idx <- grep("^cal", tolower(rawData$sampleID))
-  rawData$sampleType[idx] <- "cal"
-  
-  idx <- grep("sltr|^ltr|^pqc|^qc|^cal|blank", tolower(rawData$sampleID), invert = T)
-  rawData$sampleType[idx] <- "sample"
-  
-  rawData$sampleType <- as.factor(rawData$sampleType)
-  
-  sample_types <- paste(levels(rawData$sampleType), collapse = ", ")
-  
-  cat(bold(blue("The following sample types were found: ")) %+% 
-        bold(blue(sample_types)), fill = TRUE)
-  
-  #############sampleMatrixType##########
-  rawData$sampleMatrixType <- ifelse(grepl("PLA", rawData$AnalysisName), "PLA",
-                                     ifelse(grepl("URI", rawData$AnalysisName), "URI",
-                                            ifelse(grepl("SER", rawData$AnalysisName), "SER", NA)))
-  
-  #######cleaning LTR and double blank sampleIDs########
-  rawData$sampleID <- gsub(" |-PLA|-URI|-SER", "", rawData$sampleID)
-  
-  return(rawData)
+    
+    cat(paste("fusion:", nrow(rawData),
+              "line(s) read\n"))
+    
+    fi <- !is.na(rawData$AnalyteName)
+    rawData <- rawData[fi,]
+    cat(paste("fusion:",
+              sum(!fi),
+              "empty line(s) removed\n"))
+    
+    ####### get sampleID position in title#######
+    if ("codePosition" %in% names(optns)) {
+      codePosition <- optns$codePosition
+    } else {
+      idx <- grep(pattern = "cal", x = tolower(rawData$AnalysisName))[1]
+      pos <- strsplit(rawData[idx,"AnalysisName"], "_")[[1]]
+      codePosition <- grep(pattern = "cal", x = tolower(pos))
+    }
+    
+    #####number of compounds############
+    compoundList <- unique(rawData$AnalyteName)
+    numberOfCompounds <- length(compoundList)
+    cat(paste("fusion:",
+              numberOfCompounds,
+              "compound(s) found\n"))
+    
+    #######number of samples############
+    cat(paste("fusion:",
+              length(unique(rawData$AnalysisName)),
+              "sample(s) found\n"))
+    
+    ###### looking for duplicated lines########
+    idx <- which(duplicated(rawData[, which(colnames(rawData) %in% c("AnalysisName", "AnalyteName"))]) |
+                   duplicated(rawData[,which(colnames(rawData) %in% c("AnalysisName", "AnalyteName"))], fromLast = TRUE))
+    
+    if (length(idx) > 0) {
+      cat(crayon::red("fusion: " %+%
+                        crayon::red(idx) %+%
+                        crayon::red(" duplicated line(s) found")),
+          fill = TRUE)
+    }
+    idx <- which(duplicated(rawData[,which(colnames(rawData) %in% c("AnalysisName", "AnalyteName"))]))
+    
+    if (length(idx) > 0) {
+      rawData <- rawData[-idx,]
+      cat(crayon::white("fusion: " %+%
+                          crayon::white(idx) %+%
+                          crayon::white(" duplicated line(s) removed")),
+          fill = TRUE)
+    }
+    
+    #############adding sampleID########
+    #double blanks, LTRs, QC and CAL need further individualizing since they are not unique yet
+    
+    rawData$sampleID <- sapply(rawData$AnalysisName, function(name) {
+      # Split by "_"
+      parts <- strsplit(name, "_")[[1]]
+      
+      # Check if "Blank" or "LTR" is in the split parts
+      if(any(grep("blank|ltr|qc|cal", tolower(parts)))){
+      # if (any(grepl("Blank|LTR", parts))) {
+        
+        # Find the position of "Blank" or "LTR" and take the part with the number after it
+        idx <- which(grepl("blank|ltr|qc|cal", tolower(parts)))
+        # idx <- which(grepl("Blank|LTR", parts))
+        return(paste(parts[idx], parts[idx + 1], sep = "_"))  # Combine "Blank" or "LTR" with the next number
+      } else {
+        # For all other cases, use the default code extraction
+        return(parts[codePosition])  # Adjust 'codePosition' as needed
+      }
+    })
+    
+    
+    #############No blank sampleTypes#########
+    idx <- which(is.na(rawData$sampleType))
+    if(length(idx) > 0){
+      cat(paste("The following ", length(idx), " sample(s) have no sampleType:", rawData[idx, "AnalysisName"]))
+    }
+    
+    #############cleaning sampleType##########
+    #rename column correctly
+    colnames(rawData)[which(colnames(rawData) == "SampleType")] <- "sampleType"
+    
+    idx <- grep("blank", tolower(rawData$sampleID))
+    rawData$sampleType[idx] <- "blank"
+    
+    idx <- grep("sltr", tolower(rawData$sampleID))
+    rawData$sampleType[idx] <- "sltr"
+    
+    idx <- grep("^ltr", tolower(rawData$sampleID))
+    rawData$sampleType[idx] <- "ltr"
+    
+    idx <- grep("^pqc", tolower(rawData$sampleID))
+    rawData$sampleType[idx] <- "pqc"
+    
+    idx <- grep("^qc", tolower(rawData$sampleID))
+    rawData$sampleType[idx] <- "qc"
+    
+    idx <- grep("^cal", tolower(rawData$sampleID))
+    rawData$sampleType[idx] <- "cal"
+    
+    idx <- grep("sltr|^ltr|^pqc|^qc|^cal|blank", tolower(rawData$sampleID), invert = T)
+    rawData$sampleType[idx] <- "sample"
+    
+    rawData$sampleType <- as.factor(rawData$sampleType)
+    
+    sample_types <- paste(levels(rawData$sampleType), collapse = ", ")
+    
+    cat(bold(blue("The following sample types were found: ")) %+% 
+          bold(blue(sample_types)), fill = TRUE)
+    
+    #############sampleMatrixType##########
+    rawData$sampleMatrixType <- ifelse(grepl("PLA", rawData$AnalysisName), "PLA",
+                                       ifelse(grepl("URI", rawData$AnalysisName), "URI",
+                                              ifelse(grepl("SER", rawData$AnalysisName), "SER", NA)))
+    
+    #######cleaning LTR and double blank sampleIDs########
+    rawData$sampleID <- gsub("PLASMA|SERUM|URINE|-PLA|-URI|-SER", "", rawData$sampleID)
+    
+    # recasting to get data
+    idx <- which(tolower(names(rawData)) %in% c("sampleid", "quantity", "analysisname", "analytename"))
+    
+    newData <-
+      dcast(rawData[, idx],
+            AnalysisName + sampleID ~ AnalyteName, 
+            value.var = "Quantity")
+    
+    aa <- 
+    list("data" = newData,
+         "meta" = rawData)
+    
+    return(aa)
 }
--- tests/testthat/test-readAA.R
+++ tests/testthat/test-readAA.R
@@ -1,9 +1,24 @@
-# test_that("empty file would returns error",{
-#   readAA("emptyFile.tsv")
-# })
 
-# test_that("readAA returns rawData",{
-#   test <- readAA(file = "~/OneDrive - Murdoch University/datasets/covid19/mauritius/rawData/MS/AA/PLA/PAI-05_COVID_MauritiusPLA/PAI-05_COVID_Mauritius PLA COVp261 full plate.TSV")
-#   
-#   expect_equal(is(test)[1], "data.frame")
-# })
+test_that("sampleID is correct on older cohorts", {
+  aa <- readAA(file = "~/git/phenological/ms-parser/inst/extdata/plaAA.TSV")
+  
+  #should have all unique sampleIDs
+  expect_equal(length(unique(aa[["data"]][["sampleID"]])), nrow(aa[["data"]]))
+  
+  #should have COV sample IDs
+  expect_true(c("COV00261") %in% aa$data$sampleID)
+
+})
+
+test_that("sampleID is correct on newer cohorts", {
+  aa <- readAA(file = "~/git/phenological/ms-parser/inst/extdata/plaAA2.TSV")
+  
+  #should have all unique sampleIDs
+  expect_equal(length(unique(aa[["data"]][["sampleID"]])), nrow(aa[["data"]]))
+  
+  #should have COV sample IDs
+  expect_equal(aa[["data"]][["sampleID"]][25], "COV17242")
+  
+})
+
+
